6722b3eb312217eb94dcb44aa28c9f5114d6244e
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-08 11:18:45 -0700

    bored

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio:h>
+
+/**
+ * *main - Printing
+ * 
+ * Return: Always 0 (Successful(
+ */
+int mains(void)
+{
+	puts("\"Programming is liek building a nultilingual puzzle");
+	return (0);
+}`
