ffa497133133bf88674d7be1488675f61fb822e6
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-10 13:31:31 -0700

    prayer

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,23 @@
-#include <stdio:h>
+#include <stdio.h>
+
+
+
 /**
- * main - entry point
  *
- * Return: 0 after printing the function
- */
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+
+{
+
+		puts("\"Programming is like building a multilingual puzzle");
+
+			return (0);
+
 }
