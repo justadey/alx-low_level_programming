0ca67dc5d6633cb9a875028e94b1198d14268cab
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-10 12:48:08 -0700

    check

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio:h>
+
 /**
- * Description : main - Prints out a string to stdout.
- * * Return: 0 if success.
+ * main - Printing
+ *
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
