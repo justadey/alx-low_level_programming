9ccdb150167e0a7199b4e9c8bd8373e443748419
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-11 12:13:26 -0700

    make sense

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabeths
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+
+	putchar('\n');
+	return (0);
+}
+
