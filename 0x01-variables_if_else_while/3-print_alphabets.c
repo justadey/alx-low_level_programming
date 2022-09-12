0eb6525472ba0bb2a56ad48ce52617484f4c9ae3
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-12 12:37:53 -0700

    y

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- *
- * Return: Always 0 (Succes)
- */
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Succes)
+ *     */
 int main(void)
 {char abc = 'a';
 	char capital = 'A';
 	for (; abc <= 'z'; abc++)
-
 	{putchar(abc);
 	}
 	for (; capital <= 'Z'; capital++)
-i	{putchar(capital);
+	{putchar(capital);
 	}
 	putchar('\n');
 	return (0);
