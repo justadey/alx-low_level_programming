30906a61913b757cea8f82f54d9ee020957b2c0e
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-12 12:40:21 -0700

    chase

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,11 +4,12 @@
  *  * main - Entry point
  *   *
  *    * Return: Always 0 (Succes)
- *     */
+ */
 int main(void)
 {char abc = 'a';
 	char capital = 'A';
 	for (; abc <= 'z'; abc++)
+
 	{putchar(abc);
 	}
 	for (; capital <= 'Z'; capital++)
