63f9e5f07439c4f1167f2b65957a36a165cfbe5a
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-12 11:38:41 -0700

    y

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
+
 /**
- * main - print possible combo of 3
- *
- * Return: returns zero at the end
- */
+ * * main - program that prints the numbers from 00 to 99
+ * * Numbers must be separated by ,followed by a space
+ * * Numbers should be printed in ascending order with 2 digits
+ * * You can only use the putchar function
+ * * Return: 0
+ * */
+
+
 int main(void)
-{
-	int i, p, m;
-	for (i = 0; i <= 9; i++)
+{int first;
+	int second;
+	int third;
+	for (first = 48; first < 58; first++)
 	{
-		for (m = 2; m<= 9; m++)
+		for (second = 49; second < 58; second++)
 		{
-			if (m > p && p > i)
-			{
-				putchar(i + '0');
-				putchar(p + '0');
+			for (third = 50; third < 58; third++)
+				if (third > second && second > first)
+				{putchar(first);
+					putchar(second);
+					putchar(third);
+					if (first != 55 || second != 56)
+					{putchar(',');
+						putchar(' ');
+					}
+				}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
 
