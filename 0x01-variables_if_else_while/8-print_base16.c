ebcd3c3781276e297746e240ff54269055bac341
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-12 13:41:06 -0700

    why

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,11 +9,11 @@ int main (void)
 {
 	char c;
 
-	for (c ='0'; c <='9'; c++)
+	for ( c ='0'; c < ='9'; c++)
 	{
 		putchar(c);
 	}
-	for (c ='a'; c <='f'; c++)
+	for ( c ='a'; c <= 'f'; c++)
 	{
 		putchar(c);
 	}
