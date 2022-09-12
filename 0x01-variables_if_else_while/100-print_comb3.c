ca49d1418dacd8f00346629e9603e43fa36ee796
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-12 13:19:05 -0700

    grace

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,11 +4,14 @@
  * * main - Prints combination of numbers
  * *
  * * Return: Always (Success)
- * */
+ */
+
 
 int main(void)
+
 {int n1;
 	int m1;
+
 	for (n1 = 48; n1 <= 56; n1++)
 	{
 		for (m1 = 49; m1 <= 57; m1++)
