f30f461ff087b6249c4033aaab99b22a7f35ef12
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-12 11:56:07 -0700

    y

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,24 +1,19 @@
 #include <stdio.h>
 
 /**
- * main - Prints a series of numbers with commas
- *
- * Returns: Always (Success);
- */
+ * *main - Prints a serie of numbers with commas
+ * *
+ * * Return: Always (Success);
+ * */
 int main(void)
-{
-	int c;
-
-	for (c ='0'; c <='9'; c++)
-	{
-		putchar(c);
-		if (c != '9')
-		{
-			putchar(',');
-			putchar('');
+{int a = '0';
+	for (; a <= '9'; a++)
+	{putchar(a);
+		if (a != '9')
+		{putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
-
 	return (0);
 }
