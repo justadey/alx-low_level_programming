958737ef1f3dc0e1c524803bbdd40975f141cea5
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-13 05:11:25 -0700

    life

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,19 @@
 #include "holberton.h"
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-		int i;
-			char string[] = "Holberton\n";
+	int i;
+	char string[] = "Holberton\n";
+	
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(string[i]);
+	}
 
-				for (i = 0; i <= 9; i++)
-						{
-									_putchar(string[i]);
-										}
-
-					return (0);
+	return (0);
 }
