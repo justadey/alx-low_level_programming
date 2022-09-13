240c9adb9c475ec23cb5a841309225d168477df2
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-13 05:18:17 -0700

    grace

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,20 @@
 #include "holberton.h"
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 0 (Success)
- *     */
+ * main.h - Entry point
+ * 
+ * Return: Always 0 (Success)
+ */
+
 int main(void)
 {
-		char *sh = "Holberton";
+	char *sh = "Holberton";
+	
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
 
-			while (*sh)
-					{
-								_putchar(*sh);
-										sh++;
-											}
-				_putchar('\n');
-
-					return (0);
+	return (0);
 }
