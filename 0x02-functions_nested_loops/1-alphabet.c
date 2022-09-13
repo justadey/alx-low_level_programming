f871d56f3f8a6dd1d6c4849e7acde9d5d8e28366
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-13 05:31:24 -0700

    akoi

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ *  * print_alphabet - print alphabet
+ *   *
+ *    * Return: void
+ *     */
+void print_alphabet(void)
+{
+		int ch = 'a';
+
+			while (ch <= 'z')
+					{
+								_putchar(ch);
+										ch++;
+											}
+				_putchar('\n');
+}
