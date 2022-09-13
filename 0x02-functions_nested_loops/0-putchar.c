e886df97997d0cf7086f20588a63566277dccfc4
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-13 05:06:48 -0700

    God

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ *  * main - Entry point
+ *   *
+ *    * Return: Always 0 (Success)
+ *     */
+int main(void)
+{
+		int i;
+			char string[] = "Holberton\n";
+
+				for (i = 0; i <= 9; i++)
+						{
+									_putchar(string[i]);
+										}
+
+					return (0);
+}
