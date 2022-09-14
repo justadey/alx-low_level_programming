86cbb695501b246baae8d12bb74c38a6df956bf5
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-14 09:28:51 -0700

    hunger

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 
 /**
- * main - Entry point
+ * main - Prints the sum of all multiples of 3 0r 5 up to 1024
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
+	int i, z = 0;
+
+	while (i < 1024)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			z += i;
+		}
+		i++;
+	}
+
+	printf(% d\n", z);
 	return (0);
 }
+
