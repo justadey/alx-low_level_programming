2d288f09b69779c8a9b236d05b46e76957b6ae7b
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-20 03:50:36 -0700

    grace

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -11,13 +11,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int inc = 0;
-	
+
 	while (*(src + inc) != '\0')
 	{
 		*(dest + inc) = *(src + inc);
 		inc++;
 	}
 	*(dest + inc) = '\0';
-	
+
 	return (dest);
 }
