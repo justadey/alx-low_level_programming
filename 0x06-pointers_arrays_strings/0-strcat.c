aa76e99537fc8438560a3d27454b7a78a807ab04
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-22 13:20:23 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *strcat(char *dest, const char *src)
 	while (dest[len])
 		len++;
 
-	for (i = 0; src[i]; != '\0' ; i++)
+	for (i = 0; src[i]; != 0; i++)
 	{
 		dest[len] = src[i];
 		len += 1;
