900072c4a7d50b821bcbc29d7fb196bf98587e26
ogedengbe <o.oluwakayode16@gmail.com>
2022-09-22 22:13:47 -0700

    akoi

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,23 +1,25 @@
-#include <stdio.h>
+#include "main.h"
 
-int main(void)
+/**
+ * _atoi - converts a string to an int
+ * @s: the string to be changed
+ *
+ * Return: the converted int
+ */
+
+int _atoi(char *s)
 {
-	int n;
-	int a[5];
-	int *p;
+	int sign = 1;
+	unsigned int num = 0;
 
-	a[2] = 1024;
-	p = &n;
-	/*
-	 * write your line of code here...
-	 * Remember:
-	 * - you are not allowed to use a
-	 *   - you are not allowed to modify p
-	 *   - only one statement
-	 *   - you are not allowed to code anything else than this line of code
-	 */
-	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
-	printf("a[2] = %d\n", a[2]);
-	return (0);
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			num = num * 10 + (*s - '0');
+		else if (num > 0)
+			break;
+	} while (*s++);
+
+	return (num * sign);
 }
