82ffce7c39b2a43e9196fb371f45fc8d9cc990bc
ogedengbe <o.oluwakayode16@gmail.com>
2023-04-13 15:22:44 -0700

    update

--- 0x1E-search_algorithms/103-exponential.c
+++ 0x1E-search_algorithms/103-exponential.c
@@ -1,12 +1,10 @@
 #include "search_algos.h"
 #include <math.h>
 
-int r_binary_search(int *array, int left, int right, int value);
-void print_search(int *array, int first, int last);
-int min(int a, int b);
+int min_value(int num1, int num2);
 
 /**
- * exponential_search - Searches value in array with Exponential search algo
+ * jump_search - Searches value in array of ints using the Jump search algo
  *
  * @array: Array to search
  *
@@ -17,121 +15,41 @@ int min(int a, int b);
  * Return: First index where value is located or -1 for NULL array
  */
 
-int exponential_search(int *array, size_t size, int value)
+int jump_search(int *array, size_t size, int value)
 {
-	int index = 1;
-
-	if (array && size)
-	{
-		/* Find first location */
-		if (array[0] == value)
-		{
-			return (0);
-		}
-		/* Get range for binary search by doubling index */
-		while (index < (int)size && array[index] <= value)
-		{
-			printf("Value checked array[%d] = [%d]\n", index, array[index]);
-			index *= 2;
-		}
-		return (r_binary_search(array, index / 2, min(index, (int)size - 1), value));
-	}
-	return (-1);
-}
-
-
-/**
- * r_binary_search - Recursively breaks array into subarrays & searches
- *
- * @array: Array to search
- *
- * @left: Search on left side of array
- *
- * @right: Search on right side of array
- *
- * @value: Value to search
- *
- * Return: First index where value is located or -1 for NULL array
- */
-
-int r_binary_search(int *array, int left, int right, int value)
-{
-
-	int middle;
-
-
-	/* If value is smaller then middle, look in first half of array */
-	if (right >= left)
-	{
-		middle = left + (right - left) / 2;
-		print_search(array, left, right);
-
-		/* If search finds value in middle, return value */
-		if (array[middle] == value)
-		{
-			return (middle);
-		}
-
-		/* If element is less than middle, search left subarray */
-		if (array[middle] > value)
-		{
-			return (r_binary_search(array, left, middle - 1, value));
-		}
-
-		/* Otherwise search for value in right subarray */
-		return (r_binary_search(array, middle + 1, right, value));
-	}
-	/* If element doesn't exist return -1 */
-	return (-1);
-}
-
-
-/**
- * print_search - Prints arrayand side of array
- *
- * @array: Array to search
- *
- * @first: left hand side of array
- *
- * @last: right hand side of array
- *
- * Return: Void
- */
-
-void print_search(int *array, int first, int last)
-{
+	/* Define jump number by square root of size */
+	int jump = sqrt(size);
 	int index = 0;
 
-	printf("Searching in array: ");
-
-	while (first <= last)
+	/* Check inputs */
+	if (!array)
 	{
-		if (index > 0)
+		return (-1);
+	}
+
+	/* Find block of array where value is found in jump_step */
+	while (index < (int)size && array[index] < value)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+		index += jump;
+	}
+
+	/* Set jumpsize to index at beginning of array block to search*/
+	index -= jump;
+
+	printf("Value found between indexes [%d] and [%d]\n", index, index + jump);
+
+	/* Value found in block of size jump_step. Linearly find value */
+	while (index <= (index + jump) && index < (int)size)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		/* Found return value at index */
+		if (array[index] == value)
 		{
-			printf(", ");
+			return (index);
 		}
-		index = first++;
-		printf("%d", array[index++]);
+		index++;
 	}
-	printf("\n");
-}
-
-
-/**
- * min - min value
- *
- * @a: First number
- *
- * @b: Second number
- *
- * Return: Integer
- */
-
-int min(int a, int b)
-{
-	if (a < b)
-	{
-		return (a);
-	}
-	return (b);
+	return (-1);
 }
